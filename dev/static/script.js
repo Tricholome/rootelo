d164b030606f80f7be50c53273b87c4b0606fffa
Tricholome <Tricholome@users.noreply.github.com>
2026-04-17 13:45:58 +0000

    deploy: a0f2f8c078d2099c3fbc72dfb3c414fd33ac471c

--- dev/static/script.js
+++ dev/static/script.js
@@ -286,7 +286,7 @@ $(document).ready(function() {
 				{ "targets": 1, "className": "player-name-cell" },
 				{ "targets": 2, "className": "streak-cell" },
 				{ "targets": 3, "className": "elo-cell" },
-				{ "targets": 4, "className": "timespan-cell" },
+				{ "targets": 4, "className": "date-cell" },
 				{ "responsivePriority": 1, "targets": [0, 1] },
 				{ "responsivePriority": 2, "targets": [2, 3] },
 				{ "responsivePriority": 3, "targets": 4 },
